3e85c730b698ec8d96162926a8894d44bf7f8c7d
Drakeen <info@howardfms.com>
2019-10-13 19:00:41 -0600

    even more changes to the portfolioEntry item function

--- javascript/layout.js
+++ javascript/layout.js
@@ -8,39 +8,28 @@ var currentYear = function () {
 }
 
 //Constructors
-/* <a href="images/myMeme.png" data-toggle="lightbox" data-gallery="hidden-images" data-title="myMeme" data-footer="A Memeilistic search tool and meme repository that's easy to use and has a cleaner, less busy UI. Using Firebase User Authentication, Node, Sequelize & MySQL, this web app allows users to find and save their own personal favorites for later retrevial or sharing.">
-    <img class="card-img-top img-fluid portfolio-link" src="images/myMeme.png" folder="myMeme" demo="https://blooming-ravine-47072.herokuapp.com/">
+/* <div class="col-lg-4">
+<a href="images/at-memory.png" data-toggle="lightbox" data-gallery="hidden-images" data-title="Adventure Time Memory Game" data-footer="A simple REACTjs appliction that uses onClick event triggers to track appliction state and test a users memory in this simple game.">
+    <img class="card-img-top img-fluid portfolio-link" src="images/at-memory.png" folder="Memory-Game" demo="git">
     <div class="card-body">
-        <h5 class="card-title portfolio-link" folder="myMeme" demo="https://blooming-ravine-47072.herokuapp.com/">myMeme</h5>
+    <h5 class="card-title portfolio-link" folder="Memory-Game" demo="git">Memory Game</h5>
     </div>
 </a>
-<div class="card-body"></div>
-*/
+</div> */
 
 function portfolioEntry(imageURL, title, footer, folder, demo) {
-    return $("<a>").attr({"href": imageURL, "data-toggle": "lightbox", "data-gallery": "hidden-images", "data-title": title, "data-footer": footer }).append(
+    return $("<div>").attr({"class": "col-lg-4"}).html(
+    $("<a>").attr({"href": imageURL, "data-toggle": "lightbox", "data-gallery": "hidden-images", "data-title": title, "data-footer": footer }).append(
         $("<img>").attr({"class": "card-img-top img-fluid portfolio-link", "src": imageURL, "folder": folder, "demo": demo })
     ).append(
         $("<div>").attr({"class": "card-body"}).html(
-            $("<h5>").attr({"class": "card-title"}).text(title)
+            $("<h5>").attr({"class": "card-title portfolio-link", "folder": folder, "demo": demo}).text(title)
         )
+      )
     )
 }
 
-var test = portfolioEntry("images/at-memory.png", "React-Tube");
-
-var reactTube = 
-    $("<a>").attr({"href": "images/at-memory.png", "data-toggle": "lightbox", "data-gallery": "hidden-images", "data-title": "test"}).append(
-        $("<img>").attr({"class": "card-img-top img-fluid portfolio-link", "src": "images/at-memory.png", "folder": "Memory-Game", "demo": "git" })
-    )
-
-// function portfolioEntry(imageURL, title, footer, folder, demo) {
-//    imageURL & " | " & title & " | " & footer & " | " & folder & " | " & demo
-// }
-
-
-// var newItem = portfolioEntry("images/at-memory.png", "React-Tube", "Test footer", "Memory-Game", "git");
-
+var test = portfolioEntry("images/at-memory.png", "React-Tube", "footer text", "Memory-Game", "git");
 
 
 //EVENT triggers
@@ -48,7 +37,7 @@ var reactTube =
 $(document).ready(function() {
     //for copywrite year
     currentYear();
-    $("#react-tube").html(test);
+    $(".portfolioRow").append(test);
     
     //for bootstrap tooltips
     $('[data-toggle="tooltip"]').tooltip();
